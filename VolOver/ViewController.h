364165c2486763f6b368648406236b1f1c6e072f
Owen McGirr <o.a.mcgirr@gmail.com>
2013-01-26 14:10:35 +0000

    Initial release

--- VolOver/ViewController.h
+++ VolOver/ViewController.h
@@ -7,7 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController : UIViewController
-
+{
+    __weak IBOutlet UIBarButtonItem *playpauseButton;
+    __weak IBOutlet UISlider *volSlider;
+    BOOL firstloadComplete; // This is for checking that the app isn't launching in viewDidLoad.
+    NSTimer *timer; // This is for checking the music playback state and returning the right style to the Play button.
+    float currentValue; // This is for returning to the original volume after Mute and Full.
+}
+- (IBAction)playpauseAct:(id)sender;
+- (IBAction)exitHelp:(id)sender;
+- (IBAction)vFullAct:(id)sender;
+- (IBAction)vMuteAct:(id)sender;
+- (IBAction)vUpAct:(id)sender;
+- (IBAction)vDownAct:(id)sender;
+- (IBAction)volChanged:(id)sender;
 @end
--- VolOver/ViewController.m
+++ VolOver/ViewController.m
@@ -17,6 +17,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    [volSlider setValue:vControl.volume];
+    currentValue = vControl.volume;
+    if (firstloadComplete == NO) {
+        if ([vControl playbackState] == MPMusicPlaybackStatePaused) {
+            [vControl setVolume:0.0];
+            [vControl play];
+            [NSThread sleepForTimeInterval:1.0f];
+            [vControl pause];
+            [vControl setVolume:volSlider.value];
+        }
+        else if ([vControl playbackState] == MPMusicPlaybackStateStopped) {
+            MPMediaQuery *everything = [[MPMediaQuery alloc] init];
+            everything = [MPMediaQuery songsQuery];
+            [vControl setQueueWithQuery:everything];
+            [vControl setVolume:0.0];
+            [vControl play];
+            [NSThread sleepForTimeInterval:1.0f];
+            [vControl pause];
+            [vControl setVolume:volSlider.value];
+        }
+        // These if statements initialise the volume.
+        timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(pbChanged) userInfo:nil repeats:YES]; // See .h for comment.
+        firstloadComplete = YES;
+    }
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +51,82 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)pbChanged { // Selector for timer.
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    if ([vControl playbackState] == MPMusicPlaybackStatePlaying) {
+        [playpauseButton setStyle:UIBarButtonItemStyleDone];
+    }
+    else if ([vControl playbackState] == MPMusicPlaybackStatePaused) {
+        [playpauseButton setStyle:UIBarButtonItemStyleBordered];
+    }
+    else if ([vControl playbackState] == MPMusicPlaybackStateStopped) {
+        [playpauseButton setStyle:UIBarButtonItemStyleBordered];
+    }
+}
+
+- (IBAction)playpauseAct:(id)sender {
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    if ([vControl playbackState] == MPMusicPlaybackStatePlaying) {
+        [vControl pause];
+    }
+    else if ([vControl playbackState] == MPMusicPlaybackStatePaused) {
+        [vControl play];
+    }
+    else if ([vControl playbackState] == MPMusicPlaybackStateStopped) {
+        MPMediaQuery *everything = [[MPMediaQuery alloc] init];
+        everything = [MPMediaQuery songsQuery];
+        [vControl setQueueWithQuery:everything];
+        [vControl play];
+    }
+}
+
+- (IBAction)exitHelp:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil]; // iPhone/iPod Touch only.
+}
+
+- (IBAction)vFullAct:(id)sender {
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    if (volSlider.value < 1.0) {
+        currentValue = volSlider.value;
+        [volSlider setValue:1.0];
+        [vControl setVolume:volSlider.value];
+    }
+    else {
+        [volSlider setValue:currentValue];
+        [vControl setVolume:volSlider.value];
+    }
+}
+
+- (IBAction)vMuteAct:(id)sender {
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    if (volSlider.value > 0.0) {
+        currentValue = volSlider.value;
+        [volSlider setValue:0.0];
+        [vControl setVolume:volSlider.value];
+    }
+    else {
+        [volSlider setValue:currentValue];
+        [vControl setVolume:volSlider.value];
+    }
+}
+
+- (IBAction)vUpAct:(id)sender {
+    [volSlider setValue:volSlider.value+0.1];
+    currentValue = volSlider.value;
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    [vControl setVolume:volSlider.value];
+}
+
+- (IBAction)vDownAct:(id)sender {
+    [volSlider setValue:volSlider.value-0.1];
+    currentValue = volSlider.value;
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    [vControl setVolume:volSlider.value];
+}
+
+- (IBAction)volChanged:(id)sender {
+    MPMusicPlayerController *vControl = [MPMusicPlayerController iPodMusicPlayer];
+    [vControl setVolume:volSlider.value];
+    currentValue = volSlider.value;
+}
 @end
